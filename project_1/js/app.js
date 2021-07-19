45bcdfc2e3ef887e8543eb48faeae7e7bea9cd71
mayankrai2505 <mayankrai25052007@gmail.com>
2021-07-20 00:48:26 +0530

    project 1

---
+++ project_1/js/app.js
@@ -0,0 +1,10 @@
+function showTime() {
+var currentTime = new Date();
+var hours = currentTime.getHours();
+var minutes = currentTime.getMinutes();
+var seconds = currentTime.getSeconds();
+var clockTime = hours + ':' + minutes + ':' + seconds + " " ;
+    document.getElementById('timecloock').innerHTML = clockTime ;
+
+}
+setInterval(showTime,1000);
